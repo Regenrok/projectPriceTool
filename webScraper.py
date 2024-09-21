750e5b9e9435a59081e892c344f9312aa9ce9f68
Regenrok <rwakefield72@gmail.com>
2024-09-21 19:09:51 -0400

    Progress on the webscraper

--- webScraper.py
+++ webScraper.py
@@ -5,14 +5,14 @@ Date: September 14, 2024
 This program scrapes the web for the project component prices and outputs them to a .json file to be handled by pandas.
 """
 
-import json
-from urllib.request import urlopen
+from urllib.request import Request, urlopen
 
-url = "http://olympus.realpython.org/profiles/aphrodite"
-page = urlopen(url)
-html_bytes = page.read()
-html = html_bytes.decode("utf-8")
+url = "https://www.fcpeuro.com/BMW-parts/323Ci/?year=2000&m=20&e=177&t=6&b=9&d=65&v="
 
-print(html)
+req = Request(url, headers = {'User-Agent': 'Chrome/129.0.0.0'})
+
+page = urlopen(req).read().decode("utf-8")
+
+print(page)
 
 # To learn webscraping, learn Beautiful Soup (bs4), Selenium, or Scrapy.
