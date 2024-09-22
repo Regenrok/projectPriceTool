852a29c5c71eb0d31d48067a4d266da9ee1fb41d
Regenrok <rwakefield72@gmail.com>
2024-09-22 09:48:30 -0400

    Further progress on webscraper

--- webScraper.py
+++ webScraper.py
@@ -6,6 +6,7 @@ This program scrapes the web for the project component prices and outputs them t
 """
 
 from urllib.request import Request, urlopen
+import re
 
 url = "https://www.fcpeuro.com/BMW-parts/323Ci/?year=2000&m=20&e=177&t=6&b=9&d=65&v="
 
@@ -13,6 +14,9 @@ req = Request(url, headers = {'User-Agent': 'Chrome/129.0.0.0'})
 
 page = urlopen(req).read().decode("utf-8")
 
-print(page)
+pattern = 'Sachs 556873KT.*?data-price=".*?"'
 
+match = re.search(pattern, page)
+
+print(match)
 # To learn webscraping, learn Beautiful Soup (bs4), Selenium, or Scrapy.
